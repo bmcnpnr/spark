50bf52182ef7f15e40bbc096725d9de8b2551bf9
Burak Mete <burakmetecanpinar@gmail.com>
2017-08-30 23:18:03 +0300

    Some Spark configurations are added

--- src/main/java/MySimpleWordCounter.java
+++ src/main/java/MySimpleWordCounter.java
@@ -5,19 +5,23 @@ import org.apache.spark.api.java.JavaSparkContext;
 import scala.Tuple2;
 
 import java.util.Arrays;
+import java.util.Map;
 
 public class MySimpleWordCounter {
     public static void main(String[] args) {
-        String appName = args[0];
-        SparkConf conf = new SparkConf().setAppName(appName).setMaster("local[2]");
+        String appName = "Word Counter";
+        SparkConf conf = new SparkConf().setAppName(appName);
+        conf.setJars(new String[]{"/root/IdeaProjects/spark/target/spark-project-1.0.jar"});
         JavaSparkContext sc = new JavaSparkContext(conf);
-        JavaRDD<String> textFile = sc.textFile(args[1]);
+        JavaRDD<String> textFile = sc.textFile(args[0]);
         System.err.println(textFile.first()); //Spark only retrieved the first line of the file(laziness)
-        JavaRDD<String> tokanizedFileData = textFile.flatMap(item -> Arrays.asList(item.split(" ")).iterator());
-        JavaPairRDD<String, Integer> countPrep = tokanizedFileData.mapToPair(item -> new Tuple2<>(item, 1));
+        JavaRDD<String> tokenizedFileData = textFile.flatMap(item -> Arrays.asList(item.split(" ")).iterator());
+        JavaPairRDD<String, Integer> countPrep = tokenizedFileData.mapToPair(item -> new Tuple2<>(item, 1));
         JavaPairRDD<String, Integer> counts = countPrep.reduceByKey((accumValue, newValue) -> accumValue + newValue);
         JavaPairRDD<String, Integer> sortedCounts = counts.sortByKey();
-        sortedCounts.saveAsTextFile("/root/IdeaProjects/spark/target");
+        sortedCounts.saveAsTextFile("/root/IdeaProjects/spark/target/output");
 
+        //second method: countByValue
+        Map<String, Long> stringLongMap = tokenizedFileData.countByValue();
     }
 }
